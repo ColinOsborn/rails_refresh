737f095bab31275d07774a0e10d544edd4a10857
Colin Osborn <colin.osborn@gmail.com>
2018-08-24 00:41:45 -0600

    implemented an artist

--- spec/features/user_creates_an_artist_spec.rb
+++ spec/features/user_creates_an_artist_spec.rb
@@ -8,8 +8,8 @@ RSpec.feature "User submits a new artist" do
 
     visit '/artists'
     click_on "New Artist"
-    fill_in "artist_name" with: artist_name
-    fill_in "artist_image_path" with: artist_image_path
+    fill_in "artist_name", with: artist_name
+    fill_in "artist_image_path", with: artist_image_path
     click_on "Create Artist"
 
     expect(page).to have_content artist_name

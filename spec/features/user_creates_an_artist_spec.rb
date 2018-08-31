e0a8401bd2c26c0792e7c0b1c3f022fc7dfce16c
Colin Osborn <colin.osborn@gmail.com>
2018-08-30 18:53:17 -0600

    sad path test creation implementing artist

--- spec/features/user_creates_an_artist_spec.rb
+++ spec/features/user_creates_an_artist_spec.rb
@@ -13,7 +13,19 @@ RSpec.feature "User submits a new artist" do
     click_on "Create Artist"
 
     expect(page).to have_content artist_name
-    expect(page).to have_content artist_image_path
+    # expect(page).to have_content artist_image_path
   end
+  context "User submits invalid data" do
+    scenario "they see an error message" do
 
+      artist_image_path   = "https://i2.wp.com/www.metalinjection.net/wp-content/uploads/2016/03/cultofluna.jpg?fit=700%2C467"
+
+      visit artists_path
+      click_on "New Artist"
+      fill_in "artist_image_path", with: artist_image_path
+      click_on "Create Artist"
+
+      expect(page).to have_content "Name cannot be left blank"
+    end
+  end
 end
